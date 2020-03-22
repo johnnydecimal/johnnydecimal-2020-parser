0855e25060502caec708cc09cd41b4857eff2088
John Noble <2020@johnny.net.au>
2020-03-22 15:53:18 +1100

    Tests! Holy cow

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,35 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+import jdDetector from './input_detector/jdDetector';
+
+/*
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+*/
+
+test('detects a correct Project', () => {
+  expect(jdDetector('101 my great project').jdType).toBe('project');
+  expect(jdDetector('101 my great project // with comment').comment).toBe(
+    'with comment'
+  );
+});
+
+test('detects a correct Area', () => {
+  expect(jdDetector('10-19 my great area').jdType).toBe('area');
+  expect(jdDetector('90-99 my great area').jdType).toBe('area');
+  expect(jdDetector('90-99 my great area // with comment').comment).toBe(
+    'with comment'
+  );
+});
+
+test('returns error on nonsense', () => {
+  expect(jdDetector('10 not a project').jdType).toBe('error');
+  expect(jdDetector('10a not a project').jdType).toBe('error');
+  expect(jdDetector('10-99 not an area').jdType).toBe('error');
+  expect(jdDetector('10-29 not an area').jdType).toBe('error');
+  expect(jdDetector('1x-19 not an area').jdType).toBe('error');
 });

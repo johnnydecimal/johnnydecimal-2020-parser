30dd3bb716b29afcd69703e87eb28a3acb2e2d0a
Johnny Noble <github-a3bd@johnny.net.au>
2020-04-22 07:56:21 +1000

    💔 userbase-already-signed-in 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import { Container, Header } from 'semantic-ui-react';
+import userbase, { Item, UserResult } from 'userbase-js';
 
-import TextAreaInput from './textAreaInput/textAreaInput';
+import Account from './account';
 import CommandLine from './commandLine/commandLine';
+import TextAreaInput from './textAreaInput/textAreaInput';
 
 import 'semantic-ui-css/semantic.min.css';
 
@@ -13,12 +22,52 @@ const App: React.FC = () => {
   // Aha, thought of a good compromise to get started. There will be a separate
   // /account page where you log in, log out, etc. ALl done there for now.
 
+  // === Userbase ===
+  const [user, setUser] = useState<UserResult>();
+  useEffect(() => {
+    userbase
+      .init({ appId: process.env.REACT_APP_USERBASE_APP_ID as string })
+      .then((session) => {
+        session.user && setUser(session.user);
+      })
+      .catch((e) => console.error(e));
+  }, []);
+  // If (user) here -- asynchronously -- then we're logged in.
+
   return (
-    <Container style={{ marginTop: '50px' }}>
-      <Header as="h1">Johnny.Decimal</Header>
-      <TextAreaInput />
-      <CommandLine />
-    </Container>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/account">Account</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <Switch>
+        <Route path="/account">
+          <Account />
+        </Route>
+        <Route path="/">
+          {user ? (
+            // Logged in
+            <Container style={{ marginTop: '50px' }}>
+              <Header as="h1">Johnny.Decimal</Header>
+              <TextAreaInput />
+              <CommandLine />
+            </Container>
+          ) : (
+            // Not logged in
+            <Redirect to="/account" />
+          )}
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
